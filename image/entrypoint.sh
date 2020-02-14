c2a7fb8c1ab7915f299443222aed258984d64679
Carl Kelso <kelso@stryd.com>
2020-02-13 16:58:04 -0800

    [#6] First pass on an entry-point
    Based on documentation found here:
    https://help.github.com/en/actions/building-actions/creating-a-docker-container-action

---
+++ image/entrypoint.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+# Copyright Stryd, Inc. February 2020 - All rights reserved.
+
+# Disallow unset variables.
+set -o nounset
+
+echo "RAINIER-BUILD"
+
+# Handle reporting reasons when we exit
+report_reason() {
+  REASON=${1:-Unspecified}
+  echo :set-output name=reason::${REASON}
+  exit 1
+}
+
+report_success() {
+  REASON=${1:-Success}
+  echo :set-output name=reason::${REASON}
+  exit 0
+}
+
+DEFAULT_EXEC=make
+EXEC=
+
+# Handle argument processing
+
+# Set when we want to consume a specific type of positional argument
+POS_NEEDED=
+
+# Set to true/false/init/multi.  
+# true  - we asked for an arg this step
+# false - we asked for an arg, and then didn't provide one.
+# init  - a special case to handle the first unspecified positional.
+# multi - we can take a sequence of arguments
+POS_FRESH=init
+POS_USE_ALL=
+POS_IMPLICIT_OK=true
+
+while [ -n "${1:-}" ] ; do
+  arg="${1}"
+  shift 1
+
+  case $arg in
+    ## --       pass the remaining arguments directly to the exec
+    -- )
+    POS_USE_ALL=true
+    break
+    ;;
+
+    ## --help   print usage info
+    --help | -h )
+    echo "Usage: $0 [options]"
+    sed -n -e "s/^[ \t]*##[ \t]*\(.*\)$/\1/p" $0
+    report_success "--help" 
+    ;;
+
+    ## --exec   set the executable (make)
+    --exec | -x )
+    POS_NEEDED=exec
+    POS_FRESH=true
+    ;;
+
+    ## --target set the target to be run
+    --target | -t )
+    POS_NEEDED=target
+    POS_FRESH=true
+    ;;
+
+    ## --no-target (the default behavior)
+    --no-target | -T )
+    TARGET=
+    ;;
+
+    ## --no-implicit-args require args to be set explicitly
+    --no-implicit-args | -A )
+    POS_IMPLICIT_OK=false
+    ;;
+
+    # Perform positional argument handling here
+    * )
+    case "${POS_NEEDED}" in
+      exec )
+        EXEC="${arg}"
+        ;;
+
+      target )
+        TARGET="${arg}"
+        ;;
+
+      * )
+        # Read in positional arguments if they haven't been provided yet.
+        if [ "${POS_IMPLICIT_OK}" != "true" ] ; then
+          report_reason "Unexpected positional argument - implicits disabled"
+        elif [ -z "${EXEC:-}" ] ; then
+          EXEC="${arg}"
+        elif [ -z "${TARGET:-}" ] ; then
+          TARGET="${arg}"
+        else
+          report_reason "Unexpected positional argument"
+        fi
+        ;;
+    esac
+    if [ "${POS_FRESH}" != "multi" ] ; then
+      POS_NEEDED=
+    fi
+    ;;
+esac
+
+if [ "${POS_FRESH}" = "false" ] && [ -n "${POS_NEEDED}" ] ; then
+  report_reason "Missing positional argument"
+fi
+if [ "${POS_FRESH}" = "true" ] ; then
+  POS_FRESH=false
+fi
+done
+
+
+# Perform the actual build
+echo :set-output name=time_start::$(date -u)
+REASON=Success
+(
+# Echo the following lines, and terminate on an error
+set -x -e 
+"${EXEC:-${DEFAULT_EXEC}}" ${TARGET:+"${TARGET}"} ${POS_USE_ALL:+"$@"}
+) ; RETVAL=$?
+echo :set-output name=time_end::$(date -u)
+if [ "${RETVAL}" -eq "0" ] ; then
+  report_success
+else
+  report_reason "Application exited with code: ${RETVAL}"
+fi
+
